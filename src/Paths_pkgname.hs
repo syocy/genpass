46bd63a4a0b749c5844be1862f2a18a380bf7484
OSANAI Kazuyoshi <osmium.k@gmail.com>
2020-03-21 17:05:54 +0900

    fix

--- src/Paths_pkgname.hs
+++ src/Paths_pkgname.hs
@@ -1,28 +1,7 @@
-module Paths_pkgname where
+module Paths_pkgname
+  ( module Paths_genpass
+  ) where
 
 -- Dummy file for flycheck to find Paths_pkgname module
 
-import Data.Version
-
-import Paths_genpass as P
-
-version :: Version
-version = P.version
-
-getBinDir :: IO FilePath
-getBinDir = P.getBinDir
-
-getLibDir :: IO FilePath
-getLibDir = P.getLibDir
-
-getDynLibDir :: IO FilePath
-getDynLibDir = P.getDynLibDir
-
-getDataDir :: IO FilePath
-getDataDir = P.getDataDir
-
-getLibexecDir :: IO FilePath
-getLibexecDir = P.getLibexecDir
-
-getSysconfDir :: IO FilePath
-getSysconfDir = P.getSysconfDir
+import Paths_genpass
